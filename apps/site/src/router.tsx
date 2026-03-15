4dd102eb69396fad0cdd00af184e808a525eec00
Marvin <hi@marvr.me>
2026-03-16 00:20:41 +0100

    fix wrong routeTree import

--- apps/site/src/router.tsx
+++ apps/site/src/router.tsx
@@ -1,6 +1,6 @@
 import { createRouter } from '@tanstack/react-router'
 import { DefaultCatchBoundary, NotFound } from '~/components/index.tsx'
-import { routeTree } from '~/routeTree.gen.ts'
+import { routeTree } from '~/route-tree.gen.ts'
 
 export function getRouter() {
   const router = createRouter({
